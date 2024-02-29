34f678a07e418c7cd1f7fe84d2394a2ddb13f1a2
arunabhnewar <newardevil@gmail.com>
2024-02-29 20:15:17 +0600

    feat navbar, mobile navbar add and responsive

--- js/main.js
+++ js/main.js
@@ -16,3 +16,83 @@
 //     e.preventDefault();
 //   }
 // };
+
+// service menu dropdown event
+document.addEventListener("DOMContentLoaded", function () {
+  // Get all dropdown buttons
+  var dropdowns = document.querySelectorAll(".dropbtn");
+
+  // Add click event listener to each dropdown button
+  dropdowns.forEach(function (dropdown) {
+    dropdown.addEventListener("click", function (event) {
+      var dropdownContent = this.nextElementSibling;
+      // Toggle the display of dropdown content
+      dropdownContent.style.display =
+        dropdownContent.style.display === "block" ? "none" : "block";
+    });
+  });
+
+  // Close dropdown content when clicking outside of it
+  window.addEventListener("click", function (event) {
+    if (!event.target.matches(".dropbtn")) {
+      var dropdowns = document.querySelectorAll(".dropdown-content");
+      dropdowns.forEach(function (dropdown) {
+        dropdown.style.display = "none";
+      });
+    }
+  });
+});
+
+//responsive mobile menu toggle button event
+document.addEventListener("DOMContentLoaded", function () {
+  const menuTogglerBtn = document.querySelector(".menu-toggler-btn button");
+  const mobileMenu = document.querySelector(".mobile-menu");
+
+  // Function to close the mobile menu
+  function closeMobileMenu() {
+    mobileMenu.classList.remove("show");
+  }
+
+  // Event listener for the menu toggler button
+  menuTogglerBtn.addEventListener("click", function () {
+    mobileMenu.classList.toggle("show");
+  });
+
+  // Event listener for clicks outside the mobile menu
+  document.addEventListener("click", function (event) {
+    if (
+      !menuTogglerBtn.contains(event.target) &&
+      !mobileMenu.contains(event.target)
+    ) {
+      closeMobileMenu();
+    }
+  });
+});
+
+//
+document.addEventListener("DOMContentLoaded", function () {
+  // Get all mobile dropdown buttons
+  var mobileDropdowns = document.querySelectorAll(".mobile-dropbtn");
+
+  // Add click event listener to each mobile dropdown button
+  mobileDropdowns.forEach(function (dropdown) {
+    dropdown.addEventListener("click", function (event) {
+      var mobileDropdownContent = this.nextElementSibling;
+      // Toggle the display of mobile dropdown content
+      mobileDropdownContent.style.display =
+        mobileDropdownContent.style.display === "block" ? "none" : "block";
+    });
+  });
+
+  // Close mobile dropdown content when clicking outside of it
+  window.addEventListener("click", function (event) {
+    if (!event.target.matches(".mobile-dropbtn")) {
+      var mobileDropdowns = document.querySelectorAll(
+        ".mobile-dropdown-content"
+      );
+      mobileDropdowns.forEach(function (dropdown) {
+        dropdown.style.display = "none";
+      });
+    }
+  });
+});

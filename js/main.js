ea38fa82ac3249ce5d8d16acecd7bb5883587001
arunabhnewar <newardevil@gmail.com>
2024-02-29 14:16:53 +0600

    add some ne html file

--- js/main.js
+++ js/main.js
@@ -1,18 +1,18 @@
 // disable context menu when click on right button (off this code in development stage)
-document.addEventListener("contextmenu", function (e) {
-  e.preventDefault();
-});
+// document.addEventListener("contextmenu", function (e) {
+//   e.preventDefault();
+// });
 
 // Prevents specific key combinations from triggering their default browser behaviors.
-document.onkeydown = (e) => {
-  if (
-    e.key == 123 ||
-    (e.ctrlKey && e.shiftKey && e.key == "I") ||
-    (e.ctrlKey && e.shiftKey && e.key == "C") ||
-    (e.ctrlKey && e.shiftKey && e.key == "J") ||
-    (e.ctrlKey && e.key == "U") ||
-    e.key == "F12"
-  ) {
-    e.preventDefault();
-  }
-};
+// document.onkeydown = (e) => {
+//   if (
+//     e.key == 123 ||
+//     (e.ctrlKey && e.shiftKey && e.key == "I") ||
+//     (e.ctrlKey && e.shiftKey && e.key == "C") ||
+//     (e.ctrlKey && e.shiftKey && e.key == "J") ||
+//     (e.ctrlKey && e.key == "U") ||
+//     e.key == "F12"
+//   ) {
+//     e.preventDefault();
+//   }
+// };

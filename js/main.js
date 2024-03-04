11935ebaf346e43927bf671285760fb6598f7f60
arunabhnewar <newardevil@gmail.com>
2024-03-04 09:40:57 +0600

    feat: clean code

--- js/main.js
+++ js/main.js
@@ -1,22 +1,3 @@
-// disable context menu when click on right button (off this code in development stage)
-// document.addEventListener("contextmenu", function (e) {
-//   e.preventDefault();
-// });
-
-// Prevents specific key combinations from triggering their default browser behaviors.
-// document.onkeydown = (e) => {
-//   if (
-//     e.key == 123 ||
-//     (e.ctrlKey && e.shiftKey && e.key == "I") ||
-//     (e.ctrlKey && e.shiftKey && e.key == "C") ||
-//     (e.ctrlKey && e.shiftKey && e.key == "J") ||
-//     (e.ctrlKey && e.key == "U") ||
-//     e.key == "F12"
-//   ) {
-//     e.preventDefault();
-//   }
-// };
-
 //responsive mobile menu toggle button event
 document.addEventListener("DOMContentLoaded", function () {
   const menuTogglerBtn = document.querySelector(".menu-toggler-btn button");
